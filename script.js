b3ab60e9eee4b59ff6c0454cb8bacf67211719d5
Viihvq <vitvquintao@gmail.com>
2021-09-08 12:21:48 -0300

    Amarelinho

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ let food = {
 }
 
 function criarBG(){
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "#FFD700";
     context.fillRect(0,0,16*box, 16* box); /* posição x e y, altura e largura  */
 }
 
 function criarCobrinha(){
     for(i=0; i<snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "#FF8C00";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood(){
-    context.fillStyle = "red";
+    context.fillStyle = "#FFFFFF";
     context.fillRect(food.x, food.y, box, box);
 }
 
